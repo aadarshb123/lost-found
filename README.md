bc59c622cba1857344bf5492a1fd16e49f80d201
Aadarsh Battula <aadarshb123@gmail.com>
2025-01-29 12:18:10 -0500

    created react app using vite

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # lost&found
+
+
+Setup: 
+- Run "npm install" while in the lost-found directory
+- cd into frontend folder and run "npm run dev" in terminal/command prompt
+- run "npm install express cors mongoose dotenv" in the backend folder
+- cd into backend folder and run node server.js in a separate terminal 
---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("MERN Backend Running");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
