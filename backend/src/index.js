9e8912feae9175ff8dd02bed186c9ba145dc89dd
Sanjjith Dineshkumar <sanjjith.dinesh79@gmail.com>
2025-02-02 17:53:31 -0500

    created basic backend setup and routes, and db connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import cors from "cors";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
@@ -11,10 +13,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("MERN Backend Running");
-});
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
+  connectDB();
 });
