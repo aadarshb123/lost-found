cf61f82786c24e20f435b2ce282fc33f7aca51c8
David Cho <63265252+davidch2020@users.noreply.github.com>
2025-02-10 11:31:24 -0500

    added logo placeholder and changed halo colors

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,22 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import placeholder from './assets/Placeholder.svg'
 import './App.css'
 
-function Home() {
+function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={placeholder} className="lost_found_logo" alt="Lost and found logo" />
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React !!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -32,4 +28,4 @@ function Home() {
   )
 }
 
-export default Home
+export default App
