380290c565fc00685b853de0c698d44735b1b608
Sanjjith Dineshkumar <sanjjith.dinesh79@gmail.com>
2025-02-23 22:27:20 -0500

    added new files for item and finalized item schema

--- backend/src/models/item.model.js
+++ backend/src/models/item.model.js
@@ -2,16 +2,18 @@ import mongoose from "mongoose";
 
 //item schema:
 const itemSchema = new mongoose.Schema({
+    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
     name: { type: String, required: true },
     description: { type: String, required: true },
     location: { type: String, required: true },
     dateLost: { type: Date, required: true },
     dateFound: { type: Date, required: true },
-    imageUrl: { type: String }
-    //lost or found type: boolean
-    //type of item, 
-    //need to filter by type, location, date found/lost
-  
+    imageUrl: { type: String },
+    status: {type: String, required: true, Enum: ['lost', 'found', 'claimed']},
+    itemType: {type: String, required: true, Enum: ['electronics', 'clothing', 'personal', 'other']},
+    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now } //for updating item
   });
 
 const Item = mongoose.model("Item", itemSchema);
